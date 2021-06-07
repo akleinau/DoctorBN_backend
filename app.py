b3039b9fd5ac39d654d4e8edb4b536de4d0b760d
akleinau <kleinau@ovgu.de>
2021-06-07 10:25:12 +0200

    update file path

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from py_src.Network import Network
 from py_src.Scenario import Scenario
 import os
 
-NETWORK_FOLDER = './Networks'
+NETWORK_FOLDER = '/app/Networks'
 ALLOWED_EXTENSIONS = ['.bif']
 TEMPLATE_FOLDER = os.path.abspath('./src')
 app = Flask(__name__, template_folder=TEMPLATE_FOLDER)
