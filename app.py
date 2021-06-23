342ac648cf0c9a9819be13d114c553ef25d1ac7b
akleinau <kleinau@ovgu.de>
2021-06-23 10:20:37 +0200

    add feedback test

--- app.py
+++ app.py
@@ -184,6 +184,23 @@ def openNetwork():
     return ''
 """
 
+@app.route('/sendFeedback')
+def sendFeedback():
+
+
+    url = os.environ['TRUSTIFI_URL']+'/api/i/v1/email'
+
+    payload = "{\"recipients\":[{\"email\":\"" + os.environ['MAIL'] + "\"}],\"title\":\"Hello\",\"html\":\"Hello World\"}"
+    headers = {
+      'x-trustifi-key': os.environ['TRUSTIFI_KEY'],
+      'x-trustifi-secret': os.environ['TRUSTIFI_SECRET'],
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request('POST', url, headers = headers, data = payload)
+    print(response.json())
+
+
 
 @app.route('/')
 def home():
