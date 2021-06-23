a1cc317e5758274aa70a8626e2332e5e1f1d2327
akleinau <kleinau@ovgu.de>
2021-06-23 12:23:01 +0200

    improve feedback

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def openNetwork():
 def sendFeedback():
     data = request.get_json()
     body = data['description'] + "\n \n" + data['csv']
-    body.replace("\n", " <br> ")
+    body = body.replace("\n", " <br> ")
     print(body)
 
     url = os.environ['TRUSTIFI_URL']+'/api/i/v1/email'
