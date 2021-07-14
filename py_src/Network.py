4570db5905b8f584cbc9090d0a821d136b460f60
akleinau <kleinau@ovgu.de>
2021-07-14 14:02:52 +0200

    fix bugs

--- py_src/Network.py
+++ py_src/Network.py
@@ -3,7 +3,7 @@ import gc
 
 class Network:
 
-    #reads the model in BIF format
+    #reads the model in NET format
     def __init__(self, name):
         reader = NETReader(string=name, n_jobs=1)
         self.model = reader.get_model()
