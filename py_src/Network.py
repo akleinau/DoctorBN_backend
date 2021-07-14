44e8d160ff9f0fc1a1e4920b8083976c1f90538b
akleinau <kleinau@ovgu.de>
2021-07-14 13:43:42 +0200

    fix bugs

--- py_src/Network.py
+++ py_src/Network.py
@@ -7,6 +7,6 @@ class Network:
     def __init__(self, name):
         reader = NETReader(string=name, n_jobs=1)
         self.model = reader.get_model()
-        self.states = reader.get_states()
-        self.edges = reader.variable_edges
+        self.states = reader.state_names
+        self.edges = reader.edge_list
 
