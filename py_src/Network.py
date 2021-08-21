f2bfc99b7ce52d7a14079726087f7c43eb4c39f9
akleinau <kleinau@ovgu.de>
2021-08-21 11:40:18 +0200

    fix file format bug

--- py_src/Network.py
+++ py_src/Network.py
@@ -7,14 +7,14 @@ class Network:
     #reads the model in NET format
     def __init__(self, fileString, fileFormat):
         if fileFormat == "net":
-            reader = NETReader(string=fileString, n_jobs=1)
+            reader = NETReader(string=fileString.replace('\r\n', '\n'), n_jobs=1)
             self.model = reader.get_model()
             self.states = reader.state_names
             self.edges = reader.edge_list
             self.labels = reader.labels
 
         elif fileFormat == "bif":
-            reader = BIFReader(string=fileString, n_jobs=1)
+            reader = BIFReader(string=fileString.replace('\r\n', '\n'), n_jobs=1)
             self.model = reader.get_model()
             self.states = reader.get_states()
             self.edges = reader.variable_edges
