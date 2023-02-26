4eadd3512ade63ca928f82bf9fbd5a752970e0a6
akleinau <kleinau@ovgu.de>
2023-02-26 11:26:41 +0100

    bugfix

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL_SQL'] #can't use the original one, because support for postgres:// in URI got removed (now: postgresql)
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace("postgres", "postgresql") #can't use the original one, because support for postgres:// in URI got removed (now: postgresql)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 # NETWORK_FOLDER = './Networks'
